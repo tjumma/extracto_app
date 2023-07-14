c936e784ae491847ea0c6d72306bd382708b9307
tjumma <tim@jumma.pro>
2023-07-14 19:59:05 +0300

    Chakra dark mode by default

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,23 +6,23 @@ import { NavBar } from './components/NavBar'
 import { UnityFrame } from './views/UnityFrame'
 import { GameContextProvider } from './contexts/GameContext'
 
-const config = {
-  initialColorMode: 'dark',
-  useSystemColorMode: false,
-}
-
-export const theme = extendTheme({ config })
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
 
+  const config = {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
+  }
+
+  const theme = extendTheme({ config })
+
   return (
     <html lang="en">
       <body>
-        <ColorModeScript />
+        <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
         <CacheProvider>
           <ChakraProvider theme={theme}>
             <GameContextProvider>
