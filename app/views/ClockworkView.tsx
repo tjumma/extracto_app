3b6e9357dbba02bc095bfacd8602cfff8b5cfa32
tjumma <tim@jumma.pro>
2023-07-17 15:35:42 +0300

    Subscribe to counter account changes

--- app/views/ClockworkView.tsx
+++ app/views/ClockworkView.tsx
@@ -2,7 +2,7 @@
 
 import * as anchor from "@coral-xyz/anchor"
 import { Button, Flex, Text } from "@chakra-ui/react"
-import { FC, ReactNode, useCallback, createContext, useContext, useMemo, useEffect } from "react"
+import { FC, ReactNode, useCallback, createContext, useContext, useMemo, useEffect, useState } from "react"
 import { useWallet, useConnection, useAnchorWallet, AnchorWallet } from "@solana/wallet-adapter-react"
 import { PublicKey, SystemProgram, } from "@solana/web3.js";
 
@@ -11,11 +11,17 @@ import { ExtractoProgram, IDL } from "../../extracto_program"
 
 const COUNTER_SEED = "counter_1";
 
+type CounterDataAccount = {
+    count: number
+}
+
 export const ClockworkView: React.FC = () => {
 
     const { connection } = useConnection()
     const anchorWallet = useAnchorWallet()
     const { publicKey } = useWallet()
+
+    const [counterDataAccount, setCounterDataAccount] = useState<CounterDataAccount | null>(null)
 
     const provider = useMemo(() => new anchor.AnchorProvider(connection, anchorWallet as AnchorWallet, {})
         , [connection, anchorWallet])
@@ -38,16 +44,16 @@ export const ClockworkView: React.FC = () => {
         console.log("Fetching counter account...")
         if (counterAddress) {
             try {
-                const account = await program.account.counter.fetch(counterAddress)
-                console.log(account.count.toString())
-                // setGameDataAccount(account)
+                const counterAccount = await program.account.counter.fetch(counterAddress)
+                console.log(counterAccount.count.toString())
+                setCounterDataAccount(counterAccount)
             } catch (error) {
                 console.log(`Error fetching counter state: ${error}`)
-                // setGameDataAccount(null)
+                setCounterDataAccount(null)
             }
         }
         else {
-            // setGameDataAccount(null)
+            setCounterDataAccount(null)
         }
     }, [])
 
@@ -90,12 +96,37 @@ export const ClockworkView: React.FC = () => {
         }
     }, [counterAddress, publicKey])
 
+    useEffect(() => {
+        fetchData()
+  }, [publicKey])
+
+    useEffect(() => {
+        if (!counterAddress) return
+
+        const subscriptionId = connection.onAccountChange(
+            counterAddress,
+            (accountInfo) => {
+                const decodedAccount = program.coder.accounts.decode(
+                    "counter",
+                    accountInfo.data
+                )
+                console.log("Got new counter via socket:", decodedAccount.count.toString())
+                setCounterDataAccount(decodedAccount)
+            }
+        )
+
+        return () => {
+            connection.removeAccountChangeListener(subscriptionId)
+        }
+    }, [connection, counterAddress, program, publicKey])
+
     return (
         <Flex direction="column" px={0} py={0} alignItems={"center"}>
             <Text mt={10} mb={10}>ClockworkView</Text>
             <Button onClick={initialize} mb={5}>Initialize</Button>
             <Button onClick={increment} mb={5}>Increment</Button>
-            <Button onClick={fetchData}>Fetch</Button>
+            <Button onClick={fetchData} mb={5}>Fetch</Button>
+            <Text>{`Counter: ${counterDataAccount? counterDataAccount.count : "null"}`}</Text>
         </Flex>
     )
 }
